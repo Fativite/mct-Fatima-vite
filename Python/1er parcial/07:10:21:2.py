d41f2ddc08fad706d3e166788ed9cdc0b2aed0ac
Fativite <104381758+Fativite@users.noreply.github.com>
2022-05-31 09:45:46 -0500

    Add files via upload

---
+++ Python/1er parcial/07:10:21:2.py
@@ -0,0 +1,33 @@
+name='fatima'
+print('my name is ',name)
+
+grade='7 th'
+print('i am on',grade )
+
+group='D'
+print('on group',group)
+
+food='sushi'
+print('my favorite food is',food)
+
+like='play sports'
+print('i like to',like)
+
+
+name='sam'
+print('my name is ',name)
+
+grade='7 th'
+print('i am on',grade )
+
+group='D'
+print('on group',group)
+
+food='pasta'
+print('my favorite food is',food)
+
+like='whatch netflix'
+print('i like to',like)
+
+
+
